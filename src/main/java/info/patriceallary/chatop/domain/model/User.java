2a5e212bf247b93a0e2d70d4c09a77e2a80b2d9a
Patrice ALLARY <dev@allary-guillery.fr>
2024-01-16 09:02:51 +0100

    User & Roles Entities Implementation

---
+++ src/main/java/info/patriceallary/chatop/domain/model/User.java
@@ -0,0 +1,106 @@
+/**
+ * User Account Entity
+ */
+package info.patriceallary.chatop.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Transactional
+@Table(name = "USERS")
+public class User implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String email;
+
+    private String name;
+
+    private String password;
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+    @Column(name="updated_at")
+    private Timestamp updatedAt;
+
+
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.MERGE, CascadeType.PERSIST}
+    )
+    @JoinTable(
+            name = "USERS_ROLES",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<Role> roles = new ArrayList<>();
+
+    public User(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.createdAt = Timestamp.valueOf(LocalDate.now().atStartOfDay());
+    }
+
+    protected User() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
+
+    public void removeRole(Role role) {
+        this.roles.remove(role);
+    }
+}
